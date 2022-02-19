a78193dfc7d0062349b238ba27c28af9a69ab166
Khaled Ezzughayyar <ezzughayyar@gmail.com>
2022-02-19 21:27:18 +0200

    fix outputs description

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,12 @@
 output "acm_arn" {
-	desdescription = "ARN for certificates managed"
+	description = "ARN for certificates managed"
 	value = {
 		for key, val in aws_acm_certificate.domain_cert: key => val.arn
 	}
 }
 
 output "acm_status" {
-	dewdescription = "Status of certificates managed"
+	description = "Status of certificates managed"
 	value = {
 		for key, val in aws_acm_certificate.domain_cert: key => val.status
 	}
